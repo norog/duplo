0792fac5f5db44913e89f303f9046d82f385ec7b
norog <47349251+norog@users.noreply.github.com>
2019-02-05 01:37:57 -0800

    Update time1.sh

--- time1.sh
+++ time1.sh
@@ -3,17 +3,17 @@ echo "Start"
 while (true) 
 do
  ./75.sh;
- sleep 180; 
+ sleep 250; 
   ./50.sh;
- sleep 210; 
+ sleep 170; 
    ./80.sh;
- sleep 300; 
-   ./65.sh;
  sleep 210; 
+   ./65.sh;
+ sleep 185; 
    ./90.sh;
- sleep 180; 
+ sleep 195; 
   ./40.sh;
- sleep 180; 
+ sleep 170; 
 done;
 
 
